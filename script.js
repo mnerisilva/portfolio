09e6d220092415aeb2389ee5e5a317a94a439d8e
Marcelo Neri da Silva <marceloneri@gmail.com>
2021-07-31 15:29:09 -0300

    update

--- script.js
+++ script.js
@@ -7,91 +7,100 @@ skill_element.innerHTML = '';
 let step = 200;
 
 const arraySkills = {
-    skill_1: ['', 'F', 'r', 'e', 'e', 'l', 'a',  'n', 'c', 'e', 'r'],
+    skill_1: " Freelancer",
     skill_2: ['', 'D', 'e', 'v', 'e', 'l', 'o', 'p',  'e', 'r']
 };
 
     setTimeout(function(){
-        skill_element.innerHTML += arraySkills.skill_1[1];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,1);
     }, 200); 
     
     setTimeout(function(){
-        skill_element.innerHTML += arraySkills.skill_1[2];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,2);
     }, 400); 
     
     setTimeout(function(){
-        skill_element.innerHTML += arraySkills.skill_1[3];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,3);
     }, 600); 
     
     setTimeout(function(){
-        skill_element.innerHTML += arraySkills.skill_1[4];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,4);
     }, 800); 
     
     setTimeout(function(){
-        skill_element.innerHTML += arraySkills.skill_1[5];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,5);
     }, 1000); 
     
     setTimeout(function(){
-        skill_element.innerHTML += arraySkills.skill_1[6];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,6);
     }, 1200); 
     
     setTimeout(function(){
-        skill_element.innerHTML += arraySkills.skill_1[7];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,7);
     }, 1400); 
     
     setTimeout(function(){
-        skill_element.innerHTML += arraySkills.skill_1[8];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,8);
     }, 1600); 
     
     setTimeout(function(){
-        skill_element.innerHTML += arraySkills.skill_1[9];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,9);
     }, 1800); 
     
     setTimeout(function(){
-        skill_element.innerHTML += arraySkills.skill_1[10];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,10);
     }, 2000); 
-
-
-
     
     setTimeout(function(){
-        skill_element.innerHTML -= arraySkills.skill_1[10];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,11);
+    }, 2200); 
+    
+    
+
+
+ 
+    setTimeout(function(){
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,11);
+    }, 3800); 
+    
+    setTimeout(function(){
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,10);
     }, 4000); 
     
     setTimeout(function(){
-        skill_element.innerHTML -= arraySkills.skill_1[9];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,9);
     }, 4200); 
     
     setTimeout(function(){
-        skill_element.innerHTML -= arraySkills.skill_1[8];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,8);
     }, 4400); 
     
     setTimeout(function(){
-        skill_element.innerHTML -= arraySkills.skill_1[7];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,7);
     }, 4600); 
     
     setTimeout(function(){
-        skill_element.innerHTML -= arraySkills.skill_1[6];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,6);
     }, 4800); 
     
     setTimeout(function(){
-        skill_element.innerHTML -= arraySkills.skill_1[5];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,5);
     }, 5000); 
     
     setTimeout(function(){
-        skill_element.innerHTML -= arraySkills.skill_1[4];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,4);
     }, 5200); 
     
     setTimeout(function(){
-        skill_element.innerHTML -= arraySkills.skill_1[3];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,3);
     }, 5400); 
     
     setTimeout(function(){
-        skill_element.innerHTML -= arraySkills.skill_1[2];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,2);
     }, 5600); 
     
     setTimeout(function(){
-        skill_element.innerHTML -= arraySkills.skill_1[1];
+        skill_element.innerHTML = arraySkills.skill_1.slice(0,1);
     }, 5800); 
 
 
