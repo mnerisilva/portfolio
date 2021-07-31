f7e4968205cdf8e253089b6746ab266fcb7a930a
Marcelo Neri da Silva <marceloneri@gmail.com>
2021-07-31 16:17:44 -0300

    update

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const skill_element = document.querySelector('.home-content .t3 .skill');
 
 skill_element.innerHTML = '';
 
-let step = 200;
+let step1 = 200;
+let tmp = 2000;
 
 const arraySkills = {
     skill_1: " Freelancer",
@@ -13,94 +14,95 @@ const arraySkills = {
 
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,1);
-    }, 200); 
+    }, tmp+(1*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,2);
-    }, 400); 
+    }, tmp+(2*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,3);
-    }, 600); 
+    }, tmp+(3*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,4);
-    }, 800); 
+    }, tmp+(4*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,5);
-    }, 1000); 
+    }, tmp+(5*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,6);
-    }, 1200); 
+    }, tmp+(6*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,7);
-    }, 1400); 
+    }, tmp+(7*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,8);
-    }, 1600); 
+    }, tmp+(8*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,9);
-    }, 1800); 
+    }, tmp+(9*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,10);
-    }, 2000); 
+    }, tmp+(10*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,11);
-    }, 2200); 
+    }, tmp+(11*step1)); 
+
     
-    
+    step1 = 7000;
 
 
  
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,11);
-    }, 3800); 
+    }, tmp+(11*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,10);
-    }, 4000); 
+    }, tmp+(10*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,9);
-    }, 4200); 
+    }, tmp+(9*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,8);
-    }, 4400); 
+    }, tmp+(8*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,7);
-    }, 4600); 
+    }, tmp+(7*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,6);
-    }, 4800); 
+    }, tmp+(6*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,5);
-    }, 5000); 
+    }, tmp+(5*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,4);
-    }, 5200); 
+    }, tmp+(4*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,3);
-    }, 5400); 
+    }, tmp+(3*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,2);
-    }, 5600); 
+    }, tmp+(2*step1)); 
     
     setTimeout(function(){
         skill_element.innerHTML = arraySkills.skill_1.slice(0,1);
-    }, 5800); 
+    }, tmp+(1*step1)); 
 
 
